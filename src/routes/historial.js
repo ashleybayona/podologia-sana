fe38afd93907e741a51d405954399a42e0c91dd5
priscilacc17 <carrascalpriscila@gmail.com>
2025-06-23 03:36:06 -0500

    okei lito cerrao

--- src/routes/historial.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controller = require('../controllers/hist_med_controller');
-const validation = require('../middleware/validation')
-
-
-
-module.exports = router;
